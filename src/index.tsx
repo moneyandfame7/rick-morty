372cde9c1a798fd8f2bd6b8a80c100b2769fe892
moneyandfame7 <davidoo1234e@gmail.com>
2022-11-14 15:07:09 +0200

    feat: added function to get data from the server

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.scss"
-import App from "./App"
+import "./styles/index.scss"
+import App from "./containers/App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(<App />)
+root.render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>,
+)
