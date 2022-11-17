93a71b4f8b07d986d3f6c4d65d7dfed5a6f0f3d5
moneyandfame7 <davidoo1234e@gmail.com>
2022-11-17 22:05:30 +0200

    feat: custom hook "useErrorApi" was added

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Wrapper from "../../layouts/Wrapper";
+
+const ErrorMessage: FC = () => {
+  return (
+    <Wrapper>
+      <h1>Error</h1>
+    </Wrapper>
+  );
+};
+
+export default ErrorMessage;
--- src/pages/CharacterPage/index.tsx
+++ src/pages/CharacterPage/index.tsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from "react"
-import { getApiResource, ICharactersResponse } from "../../utils/fetch"
-import { API_CHARACTER } from "../../constants/api"
-import { ICharacter } from "../../interfaces"
-import styles from "./CharacterPage.module.scss"
-import CharacterCard from "../../components/Card"
-import Wrapper from "../../layouts/Wrapper"
-import { CircularProgress } from "@mui/material"
+import React, { useEffect, useState } from "react";
+import { getApiResource, ICharactersResponse } from "../../utils/fetch";
+import { API_CHARACTER } from "../../constants/api";
+import { ICharacter } from "../../interfaces";
+import styles from "./CharacterPage.module.scss";
+import CharacterCard from "../../components/Card";
+import Wrapper from "../../layouts/Wrapper";
+import { CircularProgress } from "@mui/material";
+import { useErrorApi } from "../../hooks/useErrorApi";
 
 const CharacterPage: React.FC = () => {
-  const [characters, setCharacters] = useState<ICharacter[]>()
-  useEffect(() => {
-    ;(async () => {
-      const res = await getApiResource<ICharactersResponse>(API_CHARACTER)
-      if (res) {
-        setCharacters(res.results)
-      }
-    })()
-  }, [])
+  const { setIsErrorApi, render } = useErrorApi();
+  const [characters, setCharacters] = useState<ICharacter[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  console.log(characters)
-  return (
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      const res = await getApiResource<ICharactersResponse>(API_CHARACTER + "1");
+
+      if (res) {
+        setCharacters(res.results);
+        setIsLoading(false);
+        setIsErrorApi(false);
+      } else {
+        setIsErrorApi(true);
+      }
+    })();
+  }, []);
+
+  console.log(characters);
+  return render(
     <Wrapper>
       <section className={styles.container}>
-        {!characters ? (
+        {isLoading ? (
           <CircularProgress />
         ) : (
-          characters.map(character => (
+          characters?.map(character => (
             <CharacterCard
               key={character.id}
               name={character.name}
@@ -38,7 +48,7 @@ const CharacterPage: React.FC = () => {
         )}
       </section>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default CharacterPage
+export default CharacterPage;
