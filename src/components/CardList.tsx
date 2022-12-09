1e30d1579fdda38afff9d77d8a74871c76911931
moneyandfame7 <davidoo1234e@gmail.com>
2022-12-09 11:18:40 +0200

    fix: lodash method "map" for render cards was added

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -2,14 +2,14 @@ import React, { FC } from "react";
 import { ICharacter } from "../interfaces";
 import Card from "./Card";
 import { Grid } from "@mui/material";
-
+import _ from "lodash";
 interface ICardListProps {
   items?: ICharacter[];
 }
 const CharacterList: FC<ICardListProps> = ({ items }) => {
   return (
     <Grid container spacing={3} alignItems='flex-end'>
-      {items?.map(item => (
+      {_.map(items, item => (
         <Card key={item.id} {...item} />
       ))}
     </Grid>
