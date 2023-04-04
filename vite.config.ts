7d54be034928720d0fb7e9baf017c4fde81be9de
moneyandfame7 <davidoo1234e@gmail.com>
2023-04-04 18:09:06 +0300

    chore: was updated and deleted dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
-import svgrPlugin from 'vite-plugin-svgr'
-import path from 'path'
+import { resolve } from 'path'
 
+const aliases = {
+  shared: 'src/shared',
+  application: 'src/application',
+  features: 'src/features'
+}
+const resolvedAliases = Object.fromEntries(
+  Object.entries(aliases).map(([key, value]) => [key, resolve(__dirname, value)])
+)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  resolve: {
+    alias: {
+      ...resolvedAliases
+    }
   }
 })
